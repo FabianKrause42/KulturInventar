cc90184f784bad664b51a63488d609394e23378f
FabianKrause42 <fabian.krause@dein-sindelfingen.de>
2026-04-01 11:33:05 +0200

    security: config.local.php aus Git-Tracking entfernt (Zugangsdaten)

--- config.local.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'db_host'    => 'database-5020045008.webspace-host.com',
-    'db_name'    => 'dbs15457954',
-    'db_user'    => 'dbu2377940',
-    'db_pass'    => 'Just4KulturInventar2SignIn',
-    'db_charset' => 'utf8mb4',
-];
